f8bb2d420fc9c1bfa8f6499f79d08ed690c1c8d3
piyapung <piyapong.mun@student.mahidol.edu>
2026-07-01 22:07:31 +0700

    Secret Fix05

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -e # stop all execution if any command fails
 # run migrations
 echo "Running migrations..."
 source app.env
-./migrate -database "postgresql://root:mypassword@postgres:5432/simple_bank?sslmode=disable" -path ./migration up
+./migrate -database "$DB_SOURCE" -path ./migration up
 
 # start the app
 echo "Starting app..."
